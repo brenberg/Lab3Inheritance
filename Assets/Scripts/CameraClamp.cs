57b3a95591cea8bc6832a636c7eb6a281bed542e
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-02 11:07:56 -0500

    Initial commit

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float cameraHeight;
+    private float XPos;
+    private float ZPos;
+    private float YRot;
+    public float posSpeed;
+    public float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHeight = 0;
+        posSpeed = 0.01f;
+        rotSpeed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z);
+
+        //OLD WAY----------------
+        //transform.position = new Vector3(XPos, cameraHeight, ZPos);
+
+        //if (playerTransform.transform.position.x - gameObject.transform.position.x >= 5f)
+        //{
+        //    XPos += PlayerScript.staticSpeed;
+        //}
+
+        //if (playerTransform.transform.position.x - gameObject.transform.position.x <= -5f)
+        //{
+        //    XPos -= PlayerScript.staticSpeed;
+        //}
+
+        //if (playerTransform.transform.position.z - gameObject.transform.position.z >= 5f)
+        //{
+        //    ZPos += PlayerScript.staticSpeed;
+        //}
+
+        //if (playerTransform.transform.position.z - gameObject.transform.position.z <= -1f)
+        //{
+        //    ZPos -= PlayerScript.staticSpeed;
+        //}
+
+
+        //NEW WAY-------------
+        //transform.position = new Vector3(XPos, 0, ZPos);
+
+        //transform.rotation = Quaternion.Euler(0, YRot, 0);
+
+        //Position
+        //if (transform.position.x < playerTransform.position.x)
+        //{
+        //    XPos += posSpeed;
+        //}
+
+        //if (transform.position.x > playerTransform.position.x)
+        //{
+        //    XPos += -posSpeed;
+        //}
+
+        //if (transform.position.z < playerTransform.position.z)
+        //{
+        //    ZPos += posSpeed;
+        //}
+
+        //if (transform.position.z > playerTransform.position.z)
+        //{
+        //    ZPos += -posSpeed;
+        //}
+
+        //Rotation
+
+        //if (transform.rotation.y < playerTransform.rotation.y)
+        //{
+        //    YRot += -rotSpeed;
+        //}
+
+        //if (transform.rotation.y > playerTransform.rotation.y)
+        //{
+        //    YRot += rotSpeed;
+        //}
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust;
+    public float moveSpeed;
+    private float YRot;
+    private float rotSpeed;
+    public static float staticSpeed;
+    public static float staticThrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 0.01f;
+        rotSpeed = 1f;
+        thrust = 4;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //OLD WAY
+        //staticSpeed = moveSpeed;
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, YRot , transform.rotation.z);
+
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed);
+        //    moveSpeed = 0.01f;
+        //}
+
+        //else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    YRot -= rotSpeed;
+        //}
+
+        //else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveSpeed);
+        //    moveSpeed = 0.01f;
+        //}
+
+        //else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    YRot += rotSpeed;
+        //}
+
+        //else
+        //{
+        //    moveSpeed = 0f;
+        //}
+
+        staticThrust = thrust;
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("FORWARD");
+            rb.AddForce(transform.forward * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            Debug.Log("FORWARD");
+            rb.AddForce(transform.forward * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+
+            rb.AddForce(transform.forward * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            rb.AddForce(transform.forward * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+
+            rb.AddForce(transform.forward * thrust);
+        }
+
+        //if(Input.GetAxis("Horizontal"))
+        //{
+
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
