0d51ca80095efec5e215ac5b4fef6a7daf45281d
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-03 13:10:11 -0500

    more work

---
+++ Assets/Scripts/HoleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "BoxA")
+        {
+            TextScript.points += 1;
+        }
+
+        if (other.gameObject.name == "BoxB")
+        {
+            TextScript.points += 3;
+        }
+
+        if (other.gameObject.name == "BoxC")
+        {
+            TextScript.points += 5;
+        }
+    }
+}
--- Assets/Scripts/TextScript.cs
+++ Assets/Scripts/TextScript.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TextScript : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
+
+    public float startTime;
+
+    public static float points;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = 120f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        startTime -= Time.deltaTime;
+        timeText.text = "Time Left:" + startTime.ToString("0");
+
+        scoreText.text = "Score: " + points.ToString();
     }
 }
